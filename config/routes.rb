bd2a95bc23982a1b33de7b86a6ebf33a8fe59be0
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-10-15 23:05:55 +1100

    Add action cable mount

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  
+  root 'rooms#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Serve websocket cable request in-process
+  mount ActionCable.server => '/cable'
 end
